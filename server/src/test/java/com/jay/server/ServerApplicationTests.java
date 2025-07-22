7f422f16882975ec4d96f4d72fb5ffe05b97020b
Jay Vallespin <jayvallespin51@gmail.com>
2025-07-22 21:40:59 +0800

    first commit

---
+++ server/src/test/java/com/jay/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jay.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
