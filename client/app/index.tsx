db99deb3f51e73a700c1a7a354b1719d2587f857
Jay Vallespin <jayvallespin51@gmail.com>
2025-07-26 11:57:51 +0800

    Fix issues

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import "../global.css";
 
 export default function Index() {
   return (
@@ -9,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>OAuth Login Page</Text>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
