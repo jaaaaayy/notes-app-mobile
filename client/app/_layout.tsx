f564be2b4e27024933da3f267bec6872d739fc4c
Jay Vallespin <jayvallespin51@gmail.com>
2025-07-26 21:00:31 +0800

    Add login and notes screen

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,5 +1,13 @@
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <GluestackUIProvider>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="notes" options={{ headerShown: false }} />
+      </Stack>
+    </GluestackUIProvider>
+  );
 }
--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,16 +1,29 @@
+import { Button, ButtonText } from "@/components/ui/button";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { useRouter } from "expo-router";
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import "../global.css";
 
 export default function Index() {
+  const router = useRouter();
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView className="flex-1 items-center justify-center gap-6">
+      <View className="flex flex-col items-center gap-2">
+        <Text className="text-2xl font-bold text-green-500">
+          Welcome to Notes
+        </Text>
+        <Text className="text-primary-500">Continue Where You Left Off.</Text>
+      </View>
+      <Button
+        variant="solid"
+        action="primary"
+        onPress={() => router.push("/notes")}
+      >
+        <Ionicons name="logo-google" size={16} color="white" />
+        <ButtonText>Continue with Google</ButtonText>
+      </Button>
+    </SafeAreaView>
   );
 }
